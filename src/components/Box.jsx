7ff48792bb072e823fe17665cb2ef06e79399941
gandalf <who7117@outlook.kr>
2019-06-20 20:41:28 +0900

    Add scale

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { css, keyframes } from 'styled-components';
 
-const move = (base1, x, rotate) => keyframes`
+const move = (base1, x, isRotate, isScale, isTransform) => keyframes`
   from {
     ${base1}: 0;
-    ${rotate ? `transform: rotate(0deg)` : ''};
+    ${isTransform ? `transform: ${isRotate ? 'rotate(0deg)' : ''}` : ''};
   }
   to {
     ${base1}: calc(${x}% - 25px);
-    ${rotate ? `transform: rotate(359deg)` : ''};
+    ${isTransform ? `transform: ${isRotate ? 'rotate(360deg)' : ''} ${isScale ? 'scale(1.5)' : ''}` : ''};
   }
 `;
 
-const diagonalMove = (base1, base2, x, rotate) => keyframes`
+const diagonalMove = (base1, base2, x, isRotate, isScale, isTransform) => keyframes`
   from {
     ${base1}: 0;
     ${base2}: 0;
-    ${rotate ? `transform: rotate(0deg)` : ''};
+    ${isTransform ? `transform: ${isRotate ? 'rotate(0deg)' : ''}` : ''};
   }
   to {
     ${base1}: calc(${x}% - 25px);
     ${base2}: calc(${x}% - 25px);
-    ${rotate ? `transform: rotate(259deg)` : ''};
+    ${isTransform ? `transform: ${isRotate ? 'rotate(360deg)' : ''} ${isScale ? 'scale(1.5)' : ''}` : ''};
   }
 `;
 
@@ -37,11 +37,11 @@ const CommonBox = styled.div`
 `;
 class Box extends React.Component {
 
-  getAnimation(x, base1, base2, bezier, time, isRotate, isDiagonal) {
+  getAnimation(x, base1, base2, bezier, time, isRotate, isDiagonal, isScale) {
     if (x) {
       const animation = isDiagonal 
-        ? css`${diagonalMove(base1, base2, x, isRotate)} ${time}s ${bezier} infinite alternate`
-        : css`${move(base1, x, isRotate)} ${time}s ${bezier} infinite alternate`;
+        ? css`${diagonalMove(base1, base2, x, isRotate, isScale, isRotate | isScale)} ${time}s ${bezier} infinite alternate`
+        : css`${move(base1, x, isRotate, isScale, isRotate | isScale)} ${time}s ${bezier} infinite alternate`;
       return animation;
     } else {
       return false;
@@ -82,11 +82,15 @@ class Box extends React.Component {
     }
   }
 
+  getTransform(isRotate, isScale) {
+
+  }
+
   render() {
-    const { type, x, base1, base2, bezier, time, step, isRotate, isDiagonal } = this.props;
+    const { type, x, base1, base2, bezier, time, step, isRotate, isDiagonal, isScale } = this.props;
     const clipPath = this.getClipPath(type);
     const cubicBezier = this.getCubicBezier(bezier);
-    const animation = this.getAnimation(x, base1, base2, cubicBezier, time, isRotate, isDiagonal);
+    const animation = this.getAnimation(x, base1, base2, cubicBezier, time, isRotate, isDiagonal, isScale);
     const timing = this.getTiming(step);
     return (
       <React.Fragment>
--- src/pages/Move.js
+++ src/pages/Move.js
@@ -30,6 +30,7 @@ class MovePage extends React.Component {
     type: "square",
     isBox: true,
     isRotate: false,
+    isScale: false,
   }
 
   handleChange = (e) => {
@@ -44,7 +45,7 @@ class MovePage extends React.Component {
   }
 
   render() {
-    const { type, isBox, isRotate } = this.state;
+    const { type, isBox, isRotate, isScale } = this.state;
 
     return (
       <ContainerComponent>
@@ -58,72 +59,76 @@ class MovePage extends React.Component {
               <input type="checkbox" id="rotate" name="isRotate" checked={isRotate} onChange={this.handleChange}/>
               <span>rotate</span>
             </label>
+            <label htmlFor="scale">
+              <input type="checkbox" id="scale" name="isScale" checked={isScale} onChange={this.handleChange}/>
+              <span>scale</span>
+            </label>
           </RemoteController>
           <Divider />
 
           <GridContainer type="row">
             <GridSection description="left 50%, 3s">
-              <Box type={type} x={50} time={3} base1="left" isRotate={isRotate} />
+              <Box type={type} x={50} time={3} base1="left" isRotate={isRotate} isScale={isScale} />
             </GridSection>
             <GridSection description="left 50%, 3s, 10 step">
-              <Box type={type} x={50} time={3} step={10} base1="left" isRotate={isRotate} />
+              <Box type={type} x={50} time={3} step={10} base1="left" isRotate={isRotate} isScale={isScale} />
             </GridSection>
             <GridSection description="left 100%, 3s">
-              <Box type={type} x={100} time={3} base1="left" isRotate={isRotate} />
+              <Box type={type} x={100} time={3} base1="left" isRotate={isRotate} isScale={isScale} />
             </GridSection>
             <GridSection description="right 100%">
-              <Box type={type} x={100} time={3} base1="right" isRotate={isRotate} />
+              <Box type={type} x={100} time={3} base1="right" isRotate={isRotate} isScale={isScale} />
             </GridSection>
           </GridContainer>
 
           <GridContainer type="column">
             <GridSection minHeight="360px" description="50%, 3s">
-              <Box type={type} x={50} time={3} base1="top" isRotate={isRotate} />
+              <Box type={type} x={50} time={3} base1="top" isRotate={isRotate} isScale={isScale} />
             </GridSection>
             <GridSection minHeight="360px" description="50%, 3s, 10 step">
-              <Box type={type} x={50} time={3} base1="top" step={10} isRotate={isRotate} />
+              <Box type={type} x={50} time={3} base1="top" step={10} isRotate={isRotate} isScale={isScale} />
             </GridSection>
             <GridSection minHeight="360px" description="50%, 3s, ease-in">
-              <Box type={type} x={50} time={3} base1="bottom" isRotate={isRotate} bezier="ease-in"/>
+              <Box type={type} x={50} time={3} base1="bottom" isRotate={isRotate} isScale={isScale} bezier="ease-in"/>
             </GridSection>
             <GridSection minHeight="360px" description="50%, 3s, 10 step">
-              <Box type={type} x={50} time={3} base1="bottom" step={10} isRotate={isRotate} />
+              <Box type={type} x={50} time={3} base1="bottom" step={10} isRotate={isRotate} isScale={isScale} />
             </GridSection>
             <GridSection minHeight="360px" description="ease-in-out">
-              <Box type={type} x={100} time={3} base1="top" isRotate={isRotate} bezier="ease-in-out"/>
+              <Box type={type} x={100} time={3} base1="top" isRotate={isRotate} isScale={isScale} bezier="ease-in-out"/>
             </GridSection>
             <GridSection minHeight="360px" description="bezier(0.075, 0.82, 0.165, 1)">
-              <Box type={type} x={100} time={3} base1="top" isRotate={isRotate} bezier={[0.075, 0.82, 0.165, 1]}/>
+              <Box type={type} x={100} time={3} base1="top" isRotate={isRotate} isScale={isScale} bezier={[0.075, 0.82, 0.165, 1]}/>
             </GridSection>
             <GridSection minHeight="360px" description="ease">
-              <Box type={type} x={100} time={3} base1="top" isRotate={isRotate} bezier="ease" />
+              <Box type={type} x={100} time={3} base1="top" isRotate={isRotate} isScale={isScale} bezier="ease" />
             </GridSection>
           </GridContainer>
       
           <GridContainer type="row" count={3}>
             <GridSection minHeight="180px" description="ease">
-              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} bezier="ease" />
+              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} isScale={isScale} bezier="ease" />
             </GridSection>
             <GridSection minHeight="180px" description="ease-in">
-              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} bezier="ease-in" />
+              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} isScale={isScale} bezier="ease-in" />
             </GridSection>
             <GridSection minHeight="180px" description="ease-in-out">
-              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} bezier="ease-in-out" />
+              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} isScale={isScale} bezier="ease-in-out" />
             </GridSection>
             <GridSection minHeight="180px" description="ease-out">
-              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} bezier="ease-out" />
+              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} isScale={isScale} bezier="ease-out" />
             </GridSection>
             <GridSection minHeight="180px" description="linear">
-              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} bezier="linear" />
+              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} isScale={isScale} bezier="linear" />
             </GridSection>
             <GridSection minHeight="180px" description="custom bezier(0.123, 0.234, 0.345, 0.456)">
-              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} bezier={[0.123, 0.234, 0.345, 0.456]} />
+              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} isScale={isScale} bezier={[0.123, 0.234, 0.345, 0.456]} />
             </GridSection>
             <GridSection minHeight="180px" description="ease-out bezier(0.075, 0.82, 0.165, 1)">
-              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} bezier={[0.075, 0.82, 0.165, 1]} />
+              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} isScale={isScale} bezier={[0.075, 0.82, 0.165, 1]} />
             </GridSection>
             <GridSection minHeight="180px" description="ease-in bezier(0.47, 0, 0.745, 0.715)">
-              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} bezier={[0.47, 0, 0.745, 0.715]} />
+              <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} isScale={isScale} bezier={[0.47, 0, 0.745, 0.715]} />
             </GridSection>
             <GridSection minHeight="180px" description="ease-in-out bezier(0.77, 0, 0.175, 1)">
               <Box type={type} x={100} time={3} base1="left" base2="top" isRotate={isRotate} isDiagonal={true} bezier={[0.77, 0, 0.175, 1]} />
